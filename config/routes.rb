63299681ffdaf478f87455519a5de8eb7bbabcf6
Jason Newman <jarnewma@gmail.com>
2019-03-11 16:53:56 +0100

    second first one

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [ :show, :new, :create, :edit ]
-  resources :subscription
+  resources :subscriptions
   resources :parasites, only: [ :new, :create, :delete ]
-  resources :payment_method
+  resources :payment_methods
 end
