f9a4434ffc5926144ec9829b272753c373b4e297
Agnese Bockane <agnese.bockane@gmail.com>
2019-03-15 16:21:20 +0100

    bug-fixes-emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < ApplicationMailer
   def notification(sub_id)
-    @subscription = Subscription.find  sub_id
+    @subscription = Subscription.find sub_id
     @user =  @subscription.user  # Instance variable => available in view
 
     mail(to: @user.email, subject: 'Notification')
     # This will render a view in `app/views/user_mailer`!
   end
 end
-
