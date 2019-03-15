904df9ea9b3fe16304b51b0a02e59e008bd92a49
Marie Lo <logemarie@gmail.com>
2019-03-15 08:14:05 -0700

    calendar link2

--- app/javascript/packs/my_calendar.js
+++ app/javascript/packs/my_calendar.js
@@ -9,20 +9,22 @@ const computeEvent = (subscription) => {
     title: subscription.name,
     start: subscription.billing_date,
     backgroundColor: computeColor(subscription),
-    url: computeUrl(subscription)
+    url: computeUrl(subscription),
+    textColor: 'white !important',
+    borderColor: 'white !important'
   }
 }
 
 const computeColor = (subscription) => {
   const categories = {
-    "News & Media": "#C42D2B",
-    "Health & Wellness": "#5CB566",
-    "Streaming": "#72C6E0",
-    "Sports & Leisure": "#20325E",
-    "Social": "#FFA544",
-    "Shopping": "#E892A3",
-    "Food": "#FFE74C",
-    "Technology": "#989C94",
+    "News & Media": "#C42D2B !important",
+    "Health & Wellness": "#5CB566 !important",
+    "Streaming": "#72C6E0 !important",
+    "Sports & Leisure": "#20325E !important",
+    "Social": "#FFA544 !important",
+    "Shopping": "#E892A3 !important",
+    "Food": "#FFE74C !important",
+    "Technology": "#989C94 !important",
     "Other": "#624763"
   }
   return categories[subscription.category]
@@ -47,7 +49,6 @@ if (calendarObject) {
   $('#calendar').fullCalendar({
     // put your options and callbacks here
     events: calendar,
-    backgroundColor: 'red'
   })
 
 
