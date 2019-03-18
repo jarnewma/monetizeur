f1d4db5ff77c403b9657848bcf499bca1e6e52ae
Jason Newman <jarnewma@gmail.com>
2019-03-18 10:44:08 +0100

    payment method align

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  create_table "payment_methods", force: :cascade do |t|
+    t.string "category"
+    t.string "card_nickname"
+    t.string "bank"
+    t.date "expiry_date"
+    t.boolean "expiry_notification"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "photo"
+
+    PaymentMethod.create(category:  "category", card_nickname: "card card_nickname",  bank: " bank",expiry_date: Date.today, user: User.last )
