3fca5f69f73ed93c16fe4aca662b441fcc4ac4ed
Nerea Pascual Diaz <nereapasc@gmail.com>
2019-03-20 20:10:56 +0100

    last changes of today

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -6,16 +6,17 @@ const initUpdateNavbarOnScroll = () => {
 
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= (window.innerHeight / 2)) {
         navbar.classList.add('navbar-lewagon-white');
-        navbar.querySelectorAll('a').forEach((a) => {
-          a.style.color = 'black'; })
+        navbar.querySelectorAll('.nav-link').forEach((a) => {
+          a.setAttribute('style', 'color: black !important;');
+        })
         logo.src = new_img
       } else {
         navbar.classList.remove('navbar-lewagon-white');
         logo.src = old_img
-        navbar.querySelectorAll('a').forEach((a) => {
-          a.style.color = 'white';
+        navbar.querySelectorAll('.nav-link').forEach((a) => {
+          a.setAttribute('style', 'color: white !important;');
         })
       }
     });
